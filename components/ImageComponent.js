35bdcb4d27fc2a4606288a883994f4daa62e0b1a
kevinsaya25@gmail.com <kevinsaya25@gmail.com>
2021-02-19 03:38:45 -0500

    https images deploy

--- components/ImageComponent.js
+++ components/ImageComponent.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function ImageComponent({ url, title, width, height }) {
   const loader = ({ src }) => {
-    return `http://http2.mlstatic.com${src}`;
+    return `https://http2.mlstatic.com${src}`;
   };
   return (
     <Image
