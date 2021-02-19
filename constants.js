af31286c02be2013bdf17b9238cc021e3343fd47
kevinsaya25@gmail.com <kevinsaya25@gmail.com>
2021-02-19 03:28:29 -0500

    API URL FIXED

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 export default {
   URL_MELI_API: "https://api.mercadolibre.com",
+  API_URL: `https://${window.location.hostname}`,
   TRADUCCIONES: {
     CONDICION: { new: "Nuevo", used: "Usado" },
   },
